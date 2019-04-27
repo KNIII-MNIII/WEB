9ff2059cedaade7c5634f2052f1fc3bc2bd3d4c4
La <podolsky1999@gmail.com>
2019-04-27 19:09:02 +0300

    Add files via upload

---
+++ ajax/отправка и получение запросов/app.js
@@ -0,0 +1,20 @@
+document.getElementById("submit").addEventListener("click", function (e) {
+    e.preventDefault();
+    // получаем данные формы
+    let registerForm = document.forms["registerForm"];
+    let userName = registerForm.elements["userName"].value;
+    let userAge = registerForm.elements["userAge"].value;
+    // сериализуем данные в json
+    let user = JSON.stringify({userName: userName, userAge: userAge});
+    alert(user);
+    let request = new XMLHttpRequest();
+    // посылаем запрос на адрес "/user"
+    request.open("POST", "http://localhost:3000/", true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.addEventListener("load", function () {
+        // получаем и парсим ответ сервера
+        let receivedUser = JSON.parse(request.response);
+        console.log(receivedUser.userName, "-", receivedUser.userAge);   // смотрим ответ сервера
+    });
+    request.send(user);
+});
