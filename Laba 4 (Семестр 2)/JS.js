ee463655b437a5a0ca7855ff213e47b897218fbe
La <podolsky1999@gmail.com>
2019-04-27 11:09:45 +0300

    Add files via upload

---
+++ Laba 4 (Семестр 2)/JS.js
@@ -0,0 +1,11 @@
+document.getElementById("r4").addEventListener("click", clicked);
+
+function clicked() {
+  var obj = {};
+  var inl = document.getElementsByTagName("input");
+  for (var i = 0; i < inl.length; i++) {
+    obj["text" + i] = inl[i].value;
+  }
+
+  console.log(obj);
+}
